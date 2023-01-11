f3723c3a1b09c90758d83eab54b8c63eafeb7619
Davyd Kryvko <davidkrivko@gmail.com>
2023-01-11 11:13:35 +0100

    Replace everything from directory

--- cinema/management/commands/wait_for_db.py
+++ cinema/management/commands/wait_for_db.py
@@ -1,7 +1,7 @@
 import time
-
+from django.db import connections
+from django.db.utils import OperationalError
 from django.core.management import BaseCommand
-from django.db import connections, OperationalError
 
 
 class Command(BaseCommand):
